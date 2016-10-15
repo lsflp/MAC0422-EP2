56a21c65307a0a3014a58cc624029024ff416035
capellaresumo <capellaresumo@gmail.com>
2016-10-15 00:05:26 -0300

    1476500726

--- testes/t1.sh
+++ testes/t1.sh
@@ -6,5 +6,21 @@ echo "> CPU ligadas!"
 COUNTER=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list)
 for i in $COUNTER;
 do
-	(ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$i "cd EP2 && ./ep2 10 3 v")
+	(for p in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; 
+	 do
+		for ii in 250 1000 4000; 
+		do
+			for jj in 5 20 80; 
+			do
+				(	echo '> ' ./ep2 $ii $jj '(' $i ')' &&
+					ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$i "cd EP2 && /usr/bin/time -v ./ep2 $ii $jj v > rtmp 2> ttmp" 2>/dev/null &&
+					scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$i:~/EP2/rtmp resultados2/r-d$ii-n$jj-p$p-$i.txt 2>/dev/null >/dev/null &&
+					scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$i:~/EP2/ttmp resultados2/t-d$ii-n$jj-p$p-$i.txt 2>/dev/null >/dev/null 
+				)
+			done
+		done
+	done) &
 done
+
+wait
+
--- testes/test.sh
+++ testes/test.sh
@@ -2,19 +2,19 @@
 #gcloud compute instances start cpu8 --zone us-east1-b
 wait
 
-CPU1=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i1)
-CPU2=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i2)
-CPU3=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i3)
+#CPU1=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i1)
+#CPU2=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i2)
+#CPU3=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i3)
 CPU4=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i4)
-CPU5=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i5)
+#CPU5=$(gcloud --format="value(networkInterfaces[0].accessConfigs[0].natIP)" compute instances list i5)
 wait
 echo "> Endereços obtidos!!"
 
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU1:~/EP2/resultados/* ./resultados  
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU2:~/EP2/resultados/* ./resultados  
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU3:~/EP2/resultados/* ./resultados  
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU1:~/EP2/resultados/* ./resultados  
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU2:~/EP2/resultados/* ./resultados  
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU3:~/EP2/resultados/* ./resultados  
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU4:~/EP2/resultados/* ./resultados  
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU5:~/EP2/resultados/* ./resultados  
+#scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no capellaresumo@$CPU5:~/EP2/resultados/* ./resultados  
 
 wait
 echo "> Finalizando"
